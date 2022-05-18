bc15e3eda1174a2900b0a3a0120d54acaaabf4b5
Lionel Mambingo <lionelmambingo@gmail.com>
2022-05-18 06:49:35 +0100

    adding code for cucumber report jenkins extension

--- src/test/java/runners/Run.java
+++ src/test/java/runners/Run.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, monochrome = true,features = {"classpath:feat/feat.feature"}
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}, monochrome = true,features = {"classpath:feat/feat.feature"}
 )
 
 public class Run  {
